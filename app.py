06cf0b24ebfbe0352f3aa23662ef1e6ad7369f8b
krishnavundakode2212 <109051587+krishnavundakode2212@users.noreply.github.com>
2026-07-06 13:28:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ employee = "  "
 with open("employees.csv", "r") as file:
     reader = csv.DictReader(file)
 
-    for row in reader:
+    for row in reader 1 :
         salary = int(row["Salary"])
         if salary > highest_salary:
             highest_salary = salary
